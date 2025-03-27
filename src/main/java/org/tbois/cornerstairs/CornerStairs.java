12a7f08947d7606c0285cb5c5e6e8d5a26bcbb17
u7w2 <max.im@live.com>
2025-03-27 01:29:06 +0000

    Initial commit

---
+++ src/main/java/org/tbois/cornerstairs/CornerStairs.java
@@ -0,0 +1,10 @@
+package org.tbois.cornerstairs;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CornerStairs implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/org/tbois/cornerstairs/mixin/CornerStairsMixin.java
@@ -0,0 +1,4 @@
+package org.tbois.cornerstairs.Mixins;
+
+public class CornerStairsMixin {
+}
